949ea2c42e53d7dc29593397c15da0d85cf6e82e
Henry Lopes <hlopes-az204@outlook.com>
2024-01-30 20:12:12 -0300

    Added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
